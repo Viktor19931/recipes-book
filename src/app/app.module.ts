d37ed99d1bafdafd7a2b967c6109f357799715d2
Viktor Hordubey <viktor_hordubey@yahoo.com>
2018-05-18 21:34:33 +0300

    added preloadingStrategy

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,11 @@ import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
-import { ShoppingListService } from './shopping-list/shopping-list.service';
 import { AppRoutingModule } from './app-routing.module';
-import { RecipeService } from './recipes/recipe.service';
-import { DataStorageService } from './shared/data-storage.service';
-import { AuthService } from './auth/auth.service';
-import { AuthGuard } from './auth/auth-guard.service';
 import { SharedModule } from './shared/shared.module';
 import { AuthModule } from './auth/auth.module';
 import { ShoppingListModule } from './shopping-list/shopping-list.module';
+import {CoreModule} from './core/core.module';
 
 @NgModule({
   declarations: [
@@ -23,9 +19,9 @@ import { ShoppingListModule } from './shopping-list/shopping-list.module';
     AppRoutingModule,
     SharedModule,
     AuthModule,
-    ShoppingListModule
+    ShoppingListModule,
+    CoreModule
   ],
-  providers: [ShoppingListService, RecipeService, DataStorageService, AuthService, AuthGuard],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
